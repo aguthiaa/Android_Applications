190387495cce1afa1827ee013a3132c0f7dea722
mwanga <aguthi.aa@gmail.com>
2019-12-01 06:56:18 +0300

    call for code

--- eCommerce/app/src/main/java/com/peter/ecommerce/MainActivity.java
+++ eCommerce/app/src/main/java/com/peter/ecommerce/MainActivity.java
@@ -2,7 +2,9 @@ package com.peter.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +17,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initializeViews();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendUserToLoginActivity();
+
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendUserToRegisterActivity();
+
+            }
+        });
     }
+
+
+
+    private void sendUserToRegisterActivity() {
+
+        Intent intent=new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    private void sendUserToLoginActivity() {
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 
 
 
