2206544b54ffc83a06e34a8a78e97e8fba55e0ee
mwanga <aguthi.aa@gmail.com>
2020-03-18 23:36:50 +0300

    Geo Fire

--- CAB/app/src/main/java/com/peter/cab/CustomerLoginRegisterActivity.java
+++ CAB/app/src/main/java/com/peter/cab/CustomerLoginRegisterActivity.java
@@ -1,18 +1,31 @@
 package com.peter.cab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomerLoginRegisterActivity extends AppCompatActivity
 {
     private TextView textTitle, dontHaveAnAccount, alreadyHaveAnAccount;
     private EditText cEmail, cPassword;
     private Button cLoginBtn, cRegisterBtn;
+
+    private ProgressDialog mDialog;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +34,8 @@ public class CustomerLoginRegisterActivity extends AppCompatActivity
         setContentView(R.layout.activity_customer_login_register);
 
         initViews();
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void initViews()
@@ -32,6 +47,8 @@ public class CustomerLoginRegisterActivity extends AppCompatActivity
         cPassword = findViewById(R.id.customer_login_password);
         cLoginBtn = findViewById(R.id.customer_login_btn);
         cRegisterBtn = findViewById(R.id.customer_register_btn);
+
+        mDialog = new ProgressDialog(this);
 
         cRegisterBtn.setVisibility(View.INVISIBLE);
         alreadyHaveAnAccount.setVisibility(View.INVISIBLE);
@@ -64,5 +81,149 @@ public class CustomerLoginRegisterActivity extends AppCompatActivity
 
             }
         });
+
+        cRegisterBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                verifyRegisterInputs();
+
+            }
+        });
+
+        cLoginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                verifyCustomerLoginInputs();
+
+            }
+        });
     }
-}
+
+
+    private void verifyCustomerLoginInputs()
+    {
+
+        mDialog.setTitle("Login.");
+        mDialog.setMessage("Please wait...");
+        mDialog.setCanceledOnTouchOutside(false);
+        mDialog.show();
+
+        String email = cEmail.getText().toString().trim();
+        String password = cPassword.getText().toString().trim();
+
+        if (!email.isEmpty())
+        {
+
+            if (!password.isEmpty())
+            {
+                LoginCustomer(email, password);
+            }
+            else
+            {
+                cPassword.setError("Password is required");
+                cPassword.requestFocus();
+                mDialog.dismiss();
+            }
+        }
+        else
+        {
+            cEmail.setError("Email is required");
+            cEmail.requestFocus();
+            mDialog.dismiss();
+        }
+
+    }
+
+    private void LoginCustomer(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(CustomerLoginRegisterActivity.this, "Customer Login successful", Toast.LENGTH_LONG).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener()
+        {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                String error = e.getMessage();
+                Toast.makeText(CustomerLoginRegisterActivity.this, error, Toast.LENGTH_LONG).show();
+                mDialog.dismiss();
+            }
+        });
+    }
+
+
+    private void verifyRegisterInputs()
+    {
+        mDialog.setTitle("Register.");
+        mDialog.setMessage("Please wait...");
+        mDialog.setCanceledOnTouchOutside(false);
+        mDialog.show();
+
+        String email = cEmail.getText().toString().trim();
+        String password = cPassword.getText().toString().trim();
+
+        if (!email.isEmpty())
+        {
+
+            if (!password.isEmpty())
+            {
+                registerNewCustomer(email, password);
+            }
+            else
+            {
+                cPassword.setError("Password is required");
+                cPassword.requestFocus();
+                mDialog.dismiss();
+            }
+        }
+        else
+        {
+            cEmail.setError("Email is required");
+            cEmail.requestFocus();
+            mDialog.dismiss();
+        }
+    }
+
+
+
+    private void registerNewCustomer(String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(CustomerLoginRegisterActivity.this, "New Customer Registered successfully", Toast.LENGTH_LONG).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener()
+        {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                String error = e.getMessage();
+
+                Toast.makeText(CustomerLoginRegisterActivity.this, error, Toast.LENGTH_LONG).show();
+                mDialog.dismiss();
+            }
+        });
+    }
+
+    }
--- CAB/app/src/main/java/com/peter/cab/DriverLoginRegisterActivity.java
+++ CAB/app/src/main/java/com/peter/cab/DriverLoginRegisterActivity.java
@@ -1,18 +1,30 @@
 package com.peter.cab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DriverLoginRegisterActivity extends AppCompatActivity
 {
     private TextView titleText, dontHaveAnAccount,haveAnAccount;
     private EditText dEmail,dPassword;
     private Button loginDriver, registerDriver;
+    private ProgressDialog mDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +33,8 @@ public class DriverLoginRegisterActivity extends AppCompatActivity
         setContentView(R.layout.activity_driver_login_register);
 
         initViews();
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void initViews()
@@ -32,6 +46,8 @@ public class DriverLoginRegisterActivity extends AppCompatActivity
         dPassword = findViewById(R.id.driver_login_password);
         loginDriver = findViewById(R.id.driver_login_btn);
         registerDriver = findViewById(R.id.driver_register_btn);
+
+        mDialog = new ProgressDialog(this);
 
         registerDriver.setVisibility(View.INVISIBLE);
         haveAnAccount.setVisibility(View.INVISIBLE);
@@ -60,6 +76,156 @@ public class DriverLoginRegisterActivity extends AppCompatActivity
                 registerDriver.setVisibility(View.INVISIBLE);
                 haveAnAccount.setVisibility(View.INVISIBLE);
 
+            }
+        });
+
+        registerDriver.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                verifyUserInputs();
+
+            }
+        });
+
+        loginDriver.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                verifyLoginInputs();
+
+            }
+        });
+    }
+
+    private void verifyLoginInputs()
+    {
+
+        mDialog.setTitle("Login.");
+        mDialog.setMessage("Please wait...");
+        mDialog.setCanceledOnTouchOutside(false);
+        mDialog.show();
+
+        String email = dEmail.getText().toString().trim();
+        String password = dPassword.getText().toString().trim();
+
+        if (!email.isEmpty())
+        {
+
+            if (!password.isEmpty())
+            {
+                loginDriver(email, password);
+            }
+            else
+            {
+                dPassword.setError("Password is required");
+                dPassword.requestFocus();
+                mDialog.dismiss();
+            }
+        }
+        else
+        {
+            dEmail.setError("Email is required");
+            dEmail.requestFocus();
+            mDialog.dismiss();
+        }
+
+
+    }
+
+    private void loginDriver(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                       if (task.isSuccessful())
+                       {
+                           Toast.makeText(DriverLoginRegisterActivity.this, "Driver Login successful", Toast.LENGTH_LONG).show();
+
+                           Intent intent = new Intent(DriverLoginRegisterActivity.this, DriversMapActivity.class);
+                           intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                           startActivity(intent);
+                           finish();
+
+                           mDialog.dismiss();
+                       }
+                    }
+                }).addOnFailureListener(new OnFailureListener()
+        {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                String error = e.getMessage();
+                Toast.makeText(DriverLoginRegisterActivity.this, error, Toast.LENGTH_LONG).show();
+                mDialog.dismiss();
+            }
+        });
+    }
+
+
+    private void verifyUserInputs()
+    {
+        mDialog.setTitle("Register.");
+        mDialog.setMessage("Please wait...");
+        mDialog.setCanceledOnTouchOutside(false);
+        mDialog.show();
+
+        String email = dEmail.getText().toString().trim();
+        String password = dPassword.getText().toString().trim();
+
+        if (!email.isEmpty())
+        {
+
+            if (!password.isEmpty())
+            {
+                registerNewDriver(email, password);
+            }
+            else
+            {
+                dPassword.setError("Password is required");
+                dPassword.requestFocus();
+                mDialog.dismiss();
+            }
+        }
+        else
+        {
+            dEmail.setError("Email is required");
+            dEmail.requestFocus();
+            mDialog.dismiss();
+        }
+
+    }
+
+
+
+    private void registerNewDriver(String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(DriverLoginRegisterActivity.this, "New Driver Registered successfully", Toast.LENGTH_LONG).show();
+                    mDialog.dismiss();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener()
+        {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                String error = e.getMessage();
+
+                Toast.makeText(DriverLoginRegisterActivity.this, error, Toast.LENGTH_LONG).show();
+                mDialog.dismiss();
             }
         });
     }
